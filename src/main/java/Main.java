976b32e283c7f811578f763628059c54de0df08c
Martijn Gribnau <foresterre@ilumeo.com>
2015-04-30 12:30:21 +0200

    Update pom (clean up, removed JME3 for now), Added main class to test whether the maven setup works.

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Martijn on 2015-04-30.
+ */
+public class Main {
+    public static void main(String[] args) {
+        assert (false || true);
+        System.out.println("Test");
+    }
+}
