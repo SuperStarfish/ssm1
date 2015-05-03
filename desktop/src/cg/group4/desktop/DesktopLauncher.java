34835a661d777d9869316fc865b617c085773e33
Martijn Gribnau <foresterre@ilumeo.com>
2015-05-03 17:29:25 +0200

    Added basic libgdx project (+ using build management using gradle)

---
+++ desktop/src/cg/group4/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cg.group4.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cg.group4.StandUp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StandUp(), config);
+	}
+}
