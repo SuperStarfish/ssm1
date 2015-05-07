393b093953487317f3f5a394311b8d516b66ee27
Nick Cleintuar <nick@cleintuar.nl>
2015-05-07 15:39:38 +0200

    Basic start button

--- core/src/cg/group4/StandUp.java
+++ core/src/cg/group4/StandUp.java
@@ -2,18 +2,50 @@ package cg.group4;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class StandUp extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Skin skin;
+	Stage stage;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		 
+		BitmapFont font = new BitmapFont();
+		skin = new Skin();
+		skin.add("default", font);
+		
+		Pixmap pixmap = new Pixmap((int)Gdx.graphics.getWidth()/4,(int)Gdx.graphics.getHeight()/10, Pixmap.Format.RGB888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.fill();
+		skin.add("background",new Texture(pixmap));
+		
+		TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
+		textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
+		textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
+		textButtonStyle.font = skin.getFont("default");
+		skin.add("default", textButtonStyle);
+		
+		TextButton newGameButton = new TextButton("New game", skin);
+        newGameButton.setPosition(Gdx.graphics.getWidth()/2 - Gdx.graphics.getWidth()/8 , Gdx.graphics.getHeight()/2);
+        stage.addActor(newGameButton);
 	}
 
 	@Override
@@ -21,7 +53,8 @@ public class StandUp extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		stage.act();
+		stage.draw();
 		batch.end();
 	}
 	
