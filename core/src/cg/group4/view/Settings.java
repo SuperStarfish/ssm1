e4184d46c7422e354811d60dd519e9e34488a5df
sirpolomotro <ben.los@gmail.com>
2015-05-11 12:45:54 +0200

    Added view package Added switch screen functionality

---
+++ core/src/cg/group4/view/Settings.java
@@ -0,0 +1,47 @@
+package cg.group4.view;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class Settings implements Screen {
+
+	Stage stage = new Stage();
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void dispose() {
+	}
+
+}
