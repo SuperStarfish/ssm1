840f34229290f4a100194347dc2132587c0e8ef7
Jean <j.a.deleeuw@student.tudelft.nl>
2015-05-14 13:45:01 +0200

    Some changes to the Stroll events and reward and scroll screens

--- core/src/cg/group4/stroll/Stroll.java
+++ core/src/cg/group4/stroll/Stroll.java
@@ -1,17 +1,42 @@
 package cg.group4.stroll;
 
+import cg.group4.view.RewardScreen;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.Window.WindowStyle;
+
 
 public class Stroll {
 	
 	protected final float cEventChance = 0.2f;
 	protected int cEventsCompleted;
+	protected Stage cStrollStage;
 	
-	public Stroll() {
+	public Stroll(Stage strollStage) {
 		cEventsCompleted = 0;
+		cStrollStage = strollStage;
+		//this.onComplete();
+		timerUpdate();
 		//INSERT TIMER THAT COUNTS DOWN
 	}
 	
+	public void timerUpdate() {
+		WindowStyle w = new WindowStyle();
+		w.titleFont = new BitmapFont();
+		Dialog d = new Dialog("An event occured.", w);
+		d.button("Accept");
+		cStrollStage.addActor(d);
+	}
+
+	public void increaseEventsCompleted() {
+		cEventsCompleted++;
+	}
+	
 	public void onComplete() {
-		
+		((Game)Gdx.app.getApplicationListener()).setScreen(new RewardScreen(cEventsCompleted));
 	}
 }
--- core/src/cg/group4/view/RewardScreen.java
+++ core/src/cg/group4/view/RewardScreen.java
@@ -1,19 +1,73 @@
 package cg.group4.view;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 
 public class RewardScreen implements Screen {
+	
+	protected Stage cRewardStage;
+	protected SpriteBatch cRewardSpriteBatch;
+	protected Texture cRewardBackground;
+	
+	protected BitmapFont cAcceptButtonFont;
+	
+	protected int cScreenWidth, cScreenHeight, cEventsCompleted;
+	
+	public RewardScreen(int eventsCompleted) {
+		this.cEventsCompleted = eventsCompleted;
+	}
 
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
+		cRewardStage = new Stage();
+		Gdx.input.setInputProcessor(cRewardStage);
+		
+		cScreenWidth = Gdx.graphics.getWidth();
+		cScreenHeight = Gdx.graphics.getHeight();
+		
+		cRewardSpriteBatch = new SpriteBatch();
+		cRewardBackground = new Texture(Gdx.files.internal("demobackground.jpg"));
+		
+		cAcceptButtonFont = new BitmapFont();
+		TextButtonStyle acceptButtonStyle = new TextButtonStyle();
+		acceptButtonStyle.font = cAcceptButtonFont;
+		TextButton acceptButton = new TextButton("Accept", acceptButtonStyle);
+		
+		acceptButton.setPosition(cScreenWidth / 2f - acceptButton.getWidth() / 2f, cScreenHeight / 3f);
+		acceptButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+				((Game)Gdx.app.getApplicationListener()).setScreen(new MainMenu());
+            }
+        });
+		
+		cRewardStage.addActor(acceptButton);
 		
 	}
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(0, 132 / 255f, 197 / 255f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		cRewardSpriteBatch.begin();
+        cRewardSpriteBatch.draw(cRewardBackground, 0, 0);
+		cRewardSpriteBatch.end();
+		
+		cRewardStage.act();
+		
+        cRewardStage.draw();		
 	}
 
 	@Override
@@ -37,13 +91,15 @@ public class RewardScreen implements Screen {
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+		dispose();	
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		cAcceptButtonFont.dispose();
+		cRewardSpriteBatch.dispose();
+		cRewardBackground.dispose();
+		cRewardStage.dispose();
 	}
 
 }
