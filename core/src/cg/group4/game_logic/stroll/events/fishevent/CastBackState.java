35d2b8ce99e057d4add4947fc1e54a1d5baa471d
Nick Cleintuar <nick@cleintuar.nl>
2015-05-28 10:09:10 +0200

    Started with the first state of the fishing event

---
+++ core/src/cg/group4/game_logic/stroll/events/fishevent/CastBackState.java
@@ -0,0 +1,38 @@
+package cg.group4.game_logic.stroll.events.fishevent;
+
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by Nick on 27-5-2015.
+ */
+public class CastBackState implements FishEventState {
+
+    protected static final float DELTA = 2.0f;
+
+    protected boolean behind;
+
+    protected FishingStrollEvent cEvent;
+
+    public CastBackState(FishingStrollEvent event){
+        cEvent = event;
+    }
+
+    public final void processInput(final Vector3 input){
+        float movementxy = (float) Math.sqrt((Math.pow(input.x,2) + Math.pow(input.y,2)));
+        float movementzy = (float) Math.sqrt((Math.pow(input.z,2) + Math.pow(input.y,2)));
+
+        if(!behind){
+            if(input.y > 0){
+                if(movementxy > DELTA || movementzy > DELTA){
+                    behind = true;
+                }
+            }
+        }else{
+            if(input.y < 0){
+                if(movementxy > DELTA || movementzy > DELTA){
+                    cEvent.eventCompleted();
+                }
+            }
+        }
+    }
+}
---
+++ core/src/cg/group4/game_logic/stroll/events/fishevent/FishingStrollEvent.java
@@ -0,0 +1,106 @@
+package cg.group4.game_logic.stroll.events.fishevent;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import cg.group4.game_logic.stroll.events.StrollEvent;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+import cg.group4.game_logic.StandUp;
+import cg.group4.util.sensors.Accelerometer;
+import cg.group4.util.timer.Timer;
+import cg.group4.view.screen.EventScreen;
+import cg.group4.view.screen_mechanics.ScreenLogic;
+
+public class FishingStrollEvent extends StrollEvent {
+	
+	protected final static int REWARDS = 10;
+	
+	protected EventScreen cScreen;
+
+	protected Sound cCompletedTaskSound;
+
+	protected Label cLabel;
+
+	protected FishEventState cState;
+
+	protected Observer cDelayInputStartObserver;
+
+	protected Timer cDelayInputTimer;
+
+	protected Observer cDelayInputStopObserver;
+
+	protected boolean cDelayNewInput;
+
+	protected Accelerometer cAccelMeter;
+	
+	public FishingStrollEvent(){
+		super();
+        cScreen = new EventScreen();
+        cLabel = cScreen.getLabel();
+        cCompletedTaskSound = Gdx.audio.newSound(Gdx.files.internal("sounds/completedTask.wav"));
+
+        cDelayInputStartObserver = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                cDelayNewInput = true;
+            }
+        };
+
+        cDelayInputStopObserver = new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                cDelayNewInput = false;
+            }
+        };
+
+        cDelayInputTimer = new Timer("DELAYFISHEVENT", 1);
+        cDelayInputTimer.getStartSubject().addObserver(cDelayInputStartObserver);
+        cDelayInputTimer.getStopSubject().addObserver(cDelayInputStopObserver);
+        
+        cAccelMeter = new Accelerometer(StandUp.getInstance().getSensorReader());
+        cAccelMeter.filterGravity(true);
+        
+        cDelayNewInput = false;
+
+        cState = new CastBackState(this);
+	}
+	
+	@Override
+	public final void update(Observable o, Object arg) {
+		Vector3 accel = cAccelMeter.update();
+        if (!cDelayNewInput) {
+            processInput(accel);
+        }
+	}
+
+    public final void eventCompleted() {
+        clearEvent();
+    }
+
+	private void processInput(Vector3 input) {
+		cState.processInput(input);
+	}
+
+	@Override
+	public final int getReward() {
+		return REWARDS;
+	}
+
+	@Override
+	public final ScreenLogic createScreen() {
+		return cScreen;
+	}
+
+	@Override
+	protected void clearEvent() {
+		super.dispose();
+        cDelayInputTimer.getStartSubject().deleteObserver(cDelayInputStartObserver);
+        cDelayInputTimer.getStopSubject().deleteObserver(cDelayInputStopObserver);
+        cDelayInputTimer.dispose();
+		
+	}
+}
