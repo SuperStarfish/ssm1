62209b700aeb49924ff8f4f72c9cfee36d1b7482
sirpolomotro <ben.los@gmail.com>
2015-06-01 12:49:12 +0200

    Finished StandUpTest. Made helper method in StandUp.

--- core/test/cg/group4/game_logic/StandUpTest.java
+++ core/test/cg/group4/game_logic/StandUpTest.java
@@ -1,7 +1,6 @@
 package cg.group4.game_logic;
 
 import cg.group4.GdxTestRunner;
-import cg.group4.util.timer.TimeKeeper;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 import org.junit.After;
@@ -14,6 +13,7 @@ import java.util.Observer;
 
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
@@ -30,24 +30,31 @@ public class StandUpTest {
     protected StandUp cStandUp;
 
     /**
-     * TimeKeeper to handle the timers.
+     * SetUp to run before each test.
      */
-    protected TimeKeeper cTimeKeeper;
-
     @Before
     public void setUp() {
         cStandUp = StandUp.getInstance();
-        cTimeKeeper = TimeKeeper.getInstance();
-        cTimeKeeper.init();
     }
 
+    /**
+     * Teardown to run after each test/
+     */
     @After
     public void tearDown() {
-        cStandUp = null;
-        cTimeKeeper
+        StandUp.reset();
         Preferences preferences = Gdx.app.getPreferences("TIMER");
         preferences.clear();
         preferences.flush();
+    }
+
+    /**
+     * Tests the reset of the singleton..
+     */
+    @Test
+    public void testReset() {
+        StandUp.reset();
+        assertNotEquals(cStandUp, StandUp.getInstance());
     }
 
     /**
@@ -71,9 +78,16 @@ public class StandUpTest {
         assertNotNull(cStandUp.getStroll());
     }
 
+    /**
+     * Tests the removal of the stroll.
+     */
     @Test
     public void testEndStroll() {
+        cStandUp.startStroll();
 
+        assertNotNull(cStandUp.getStroll());
+        cStandUp.endStroll(0);
+        assertNull(cStandUp.getStroll());
     }
 
     /**
@@ -84,9 +98,15 @@ public class StandUpTest {
         assertEquals(cStandUp.cStroll, cStandUp.getStroll());
     }
 
+    /**
+     * Tests the update class, to update the game mechanics.
+     */
     @Test
     public void testUpdate() {
-
+        Observer mockObserver = mock(Observer.class);
+        cStandUp.getUpdateSubject().addObserver(mockObserver);
+        cStandUp.update();
+        verify(mockObserver, times(1)).update((Observable) any(), any());
     }
 
     /**
@@ -98,15 +118,18 @@ public class StandUpTest {
     }
 
     /**
-     * Tests the retrieval of the newStrollSubject.
+     * Tests the retrieval of the StrollSubject.
      */
     @Test
     public void testGetNewStrollSubject() {
         assertEquals(cStandUp.cNewStrollSubject, cStandUp.getNewStrollSubject());
     }
 
+    /**
+     * Tests the retrieval of the SensorReader.
+     */
     @Test
     public void testGetSensorReader() {
-
+        assertEquals(cStandUp.cSensorReader, cStandUp.getSensorReader());
     }
 }
