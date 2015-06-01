d17d9939038ca24011d7b61ac83fab42ea221ee5
M.M.W. Gribnau <foresterre@ilumeo.com>
2015-06-01 13:36:13 +0200

    Moved the Collection to rewards

--- core/src/cg/group4/rewards/Collection.java
+++ core/src/cg/group4/rewards/Collection.java
@@ -1,9 +1,14 @@
-package cg.group4.view.screen;
-
-import java.util.HashSet;
+package cg.group4.rewards;
 
 import cg.group4.rewards.collectibles.Collectible;
 
+import java.util.HashSet;
+
+/**
+ * Collection which can hold collectibles.
+ * A collectible exists out of an shape and colour.
+ * Only one of each collectibles can exist in one collection.
+ */
 public class Collection {
 	
 	protected HashSet<Collectible> cCollection;
