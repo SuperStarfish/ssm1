9dc4e72a8692fc3ea504d52db7cbaf0ddd4f1571
sirpolomotro <ben.los@gmail.com>
2015-06-08 22:46:21 +0200

    hue comparator

---
+++ data_structures/src/cg/group4/data_structures/collection/collectibles/collectible_comparators/HueComparator.java
@@ -0,0 +1,22 @@
+package cg.group4.data_structures.collection.collectibles.collectible_comparators;
+
+import cg.group4.data_structures.collection.collectibles.Collectible;
+
+import java.util.Comparator;
+
+/**
+ * Compares two collectibles on hue.
+ */
+public class HueComparator implements Comparator<Collectible> {
+
+    @Override
+    public int compare(Collectible o1, Collectible o2) {
+        if (o1.getHue() < o2.getHue()) {
+            return 1;
+        }
+        if (o1.getHue() == o2.getHue()) {
+            return 0;
+        }
+        return -1;
+    }
+}
