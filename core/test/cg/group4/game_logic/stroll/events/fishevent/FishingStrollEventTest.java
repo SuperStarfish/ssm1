9b8a3cd773ed238b19e33d3e7a9c2f6a083b7201
Nick Cleintuar <nick@cleintuar.nl>
2015-06-11 09:55:45 +0200

    Fixed checkstyle warnings

--- core/test/cg/group4/game_logic/stroll/events/fishevent/FishingStrollEventTest.java
+++ core/test/cg/group4/game_logic/stroll/events/fishevent/FishingStrollEventTest.java
@@ -1,6 +1,5 @@
 package cg.group4.game_logic.stroll.events.fishevent;
 
-import cg.group4.data_structures.subscribe.Subject;
 import cg.group4.util.timer.Timer;
 import com.badlogic.gdx.math.Vector3;
 import org.junit.Before;
@@ -11,7 +10,30 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Tests the FishingStrollEvent.java class and all its state classes.
+ */
 public class FishingStrollEventTest {
+
+    /**
+     * Value for the z axis to get out of the castForward state
+     */
+    protected static final float CAST_FORWARD_SUCCESS = 4f;
+
+    /**
+     * Value for the z axis to stay in the castForward state
+     */
+    protected static final float CAST_FORWARD_FAIL = 3f;
+
+    /**
+     * Value for the z axis to get out of the reelIn state
+     */
+    protected static final float REEL_IN_SUCCESS = -3f;
+
+    /**
+     * Value for the z axis to stay in the reelIn state
+     */
+    protected static final float REEL_IN_FAIL = -2f;
 
     /**
      * Instance of the stroll to be tested.
@@ -33,11 +55,17 @@ public class FishingStrollEventTest {
         Mockito.doNothing().when(cSpyEvent).clearEvent();
     }
 
+    /**
+     * The getter should return the amount of rewards.
+     */
     @Test
     public void getRewardTest() {
         assertEquals(FishingStrollEvent.REWARDS, cTestEvent.getReward());
     }
 
+    /**
+     * The event should be disposed/cleared when you complete it
+     */
     @Test
     public void eventCompletedTest() {
         cSpyEvent.eventCompleted();
@@ -45,44 +73,59 @@ public class FishingStrollEventTest {
         Mockito.verify(cSpyEvent).clearEvent();
     }
 
+    /**
+     * The event should be completed when you pull in the fish.
+     */
     @Test
     public void reelInStateProcessInputTest() {
         ReelInState state = new ReelInState(cSpyEvent);
         cSpyEvent.cState = state;
         Mockito.doNothing().when(cSpyEvent).eventCompleted();
-        state.processInput(new Vector3(0, 0, -3f));
+        state.processInput(new Vector3(0, 0, REEL_IN_SUCCESS));
 
         Mockito.verify(cSpyEvent).eventCompleted();
     }
 
+    /**
+     * The event should not be completed when you don't pull in the fish hard enough.
+     */
     @Test
     public void reelInStateProcessInputFailTest() {
         ReelInState state = new ReelInState(cSpyEvent);
         cSpyEvent.cState = state;
         Mockito.doNothing().when(cSpyEvent).eventCompleted();
-        state.processInput(new Vector3(0,0,-2f));
+        state.processInput(new Vector3(0, 0, REEL_IN_FAIL));
 
         Mockito.verify(cSpyEvent, Mockito.times(0)).eventCompleted();
     }
 
+    /**
+     * The state should change when you swing hard enough.
+     */
     @Test
     public void castForwardStateProcessInputTest() {
         CastForwardState state = new CastForwardState(cSpyEvent);
         cSpyEvent.cState = state;
-        state.processInput(new Vector3(0, 0, 4f));
+        state.processInput(new Vector3(0, 0, CAST_FORWARD_SUCCESS));
 
         assertTrue(cSpyEvent.cState instanceof WaitState);
     }
 
+    /**
+     * The state shouldn't change when you swing not hard enough.
+     */
     @Test
     public void castForwardStateProcessInputFailTest() {
         CastForwardState state = new CastForwardState(cSpyEvent);
         cSpyEvent.cState = state;
-        state.processInput(new Vector3(0, 0, 3f));
+        state.processInput(new Vector3(0, 0, CAST_FORWARD_FAIL));
 
         assertFalse(cSpyEvent.cState instanceof WaitState);
     }
 
+    /**
+     * The timer shouldn't be reset if nothing happens.
+     */
     @Test
     public void waitStateProcessInputTest() {
         WaitState state = new WaitState(cSpyEvent);
@@ -94,6 +137,9 @@ public class FishingStrollEventTest {
         Mockito.verify(state.cFishTimer, Mockito.times(0)).reset();
     }
 
+    /**
+     * The timer should reset when there is too much movement in the waitstate.
+     */
     @Test
     public void waitStateProcessInputFailTest() {
         WaitState state = new WaitState(cSpyEvent);
