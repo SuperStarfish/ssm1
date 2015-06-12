812b0e06a70f4e63f59ff3a68339d2d89023e8cb
Jean <j.a.deleeuw@student.tudelft.nl>
2015-06-12 14:47:00 +0200

    Busy with multiplayer hitboxes

--- core/src/cg/group4/game_logic/stroll/events/multiplayer/CraneFishing.java
+++ core/src/cg/group4/game_logic/stroll/events/multiplayer/CraneFishing.java
@@ -1,13 +1,19 @@
 package cg.group4.game_logic.stroll.events.multiplayer;
 
+import java.util.ArrayList;
 import java.util.Observable;
+
+import javax.swing.GroupLayout.Alignment;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 
 import cg.group4.game_logic.StandUp;
 import cg.group4.game_logic.stroll.events.StrollEvent;
@@ -17,52 +23,62 @@ import cg.group4.util.sensor.Gyroscope;
 public class CraneFishing extends StrollEvent {
 	
 	protected Accelerometer cAccelmeter;
-	protected Image cBoat, cCrane;
+	protected Stack cBoatStack;
+	protected Boat cBoat;
+	protected Crane cCrane;
 	protected CraneFishingScreen cScreen;
-	
-	private Circle craneCollisionCircle;
+	protected Image x;
+	protected int cSpeed = 4;
+	protected double cPrevAngle = 0;
+	protected ArrayList<SmallFish> fishList;
+	protected Circle cHitBox;
+	protected int cHitBoxBaseX, cHitBoxBaseY;
+	protected int cCraneOffsetX, cCraneOffsetY;
 	
 	public CraneFishing(CraneFishingScreen screen) {
 		super();
 		cAccelmeter = new Accelerometer(StandUp.getInstance().getSensorReader());
 		cAccelmeter.filterGravity(false);
-		//craneCollisionCircle = new Circle();
-		//craneCollisionCircle.setRadius(5.5f);
-		//craneCollisionCircle.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		
-		cCrane = new Image(new Texture(Gdx.files.internal("images/Crane.png")));
-		cCrane.setPosition(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-		cCrane.layout();
-		cCrane.setOrigin(cCrane.getImageWidth() / 2, cCrane.getImageHeight() / 2);
-		cScreen = screen;
-		cScreen.getWidgetGroup().addActor(cCrane);
-	}
+		cAccelmeter.setNoiseThreshold(0.5f);
+		cAccelmeter.setFilterPerAxis(false); 
 
-	@Override
-	public void update(Observable o, Object arg) {
-		Vector3 update = cAccelmeter.update();
-		float speed = 2;
+		fishList = new ArrayList<SmallFish>();
 		
-		float totalForce = Math.abs(update.x) + Math.abs(update.y);
-		System.out.println("TOTAL FORCE: " + totalForce);
+		cBoatStack = new Stack();
+		cBoatStack.add(new Image(new Texture(Gdx.files.internal("images/BoatBox.png"))));
+
+		cBoatStack.setPosition(120, 220);
+		cBoatStack.setSize(64, 64);
 		
-		float currentX = cCrane.getX();
-		float currentY = cCrane.getY(); 
+		x = new Image(new Texture(Gdx.files.internal("images/HitBox.png")));
+		x.setSize(8,8);
 		
-		float newX = currentX - speed*(update.x / totalForce);
-		float newY = currentY - speed*(update.y / totalForce);
+		setHitBoxPosition(cBoatStack, 0);
 		
-		System.out.println("OLD X: " + currentX + " OLD Y: " + currentY);
-		System.out.println("NEW X: " + (currentX + speed*(update.x / totalForce)) + " NEW Y: " + (currentY + speed*(update.y / totalForce)));
+//		cBoat = new Boat();
+//		cCrane = new Crane();
+
+//		cBoatStack.add(cBoat);
+//		cBoatStack.add(cCrane);
 		
-		if(Float.isNaN(newX)) {
-			newX = currentX;
-		}
-		if(Float.isNaN(newY)) {
-			newY = currentY;
-		}
-		cCrane.setPosition(newX, newY);
+		cScreen = screen;
+		cScreen.getWidgetGroup().debugAll();
+		cScreen.getWidgetGroup().addActor(cBoatStack);
+		cScreen.getWidgetGroup().addActor(x);
+	}
+	
+	protected void setHitBoxPosition(Stack boat, float angle) {
+		float radius = boat.getHeight() / 2;
+		float hitBoxSize = x.getWidth() / 2;
+		float xCoordCenter = boat.getX() + radius;
+		float yCoordCenter = boat.getY() + radius;
+
+		float xPlacement = (float)Math.cos(Math.toRadians(angle)) * -radius;
+		float yPlacement = (float)Math.sin(Math.toRadians(angle)) * radius;
 		
+		x.setPosition(xCoordCenter - hitBoxSize + xPlacement, yCoordCenter - hitBoxSize + yPlacement);
+		
+		System.out.println(radius);
 	}
 
 	@Override
@@ -81,6 +97,56 @@ public class CraneFishing extends StrollEvent {
 	public void start() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		Vector3 update = cAccelmeter.update();
+//		updateBoat(update);
+//		updateCrane(update);
+	}
+	
+	protected void updateBoat(Vector3 update) {
+		float totalForce = Math.abs(update.x) + Math.abs(update.y);
+		float newX = cBoat.getX();
+		float newY = cBoat.getY();
+		
+		if(totalForce != 0) {
+			newX = newX - cSpeed*(update.x / totalForce);
+			newY = newY - cSpeed*(update.y / totalForce);
+		}
+		
+		cBoat.setPosition(newX, newY);
+		cCrane.setPosition(newX + cCraneOffsetX, newY + cCraneOffsetY);
+	}
+	
+	protected void updateCrane(Vector3 update) {
+		double angle = cPrevAngle;
+		if(update.x != 0) {
+			angle = Math.toDegrees(Math.atan(update.y / update.x));
+		}
+		
+		int extraAngle = 0;
+		if(update.x > 0 && update.y > 0) {
+			extraAngle = 90;
+		} else if(update.x < 0 && update.y > 0) {
+			extraAngle = 180;
+		} else if(update.x < 0 && update.y < 0) {
+			extraAngle = 270;
+		}
+		angle += extraAngle;
+		cCrane.setRotation((float) -cPrevAngle);
+		cCrane.setRotation((float) angle);
+		cPrevAngle = angle;
+		updateHitBox((float) angle);
+	}
+	
+	protected void updateHitBox(float angle) {
+		int newX = (int) (Math.cos(angle) * (cHitBoxBaseX - (cCrane.getX() + cCrane.getOriginX())) - Math.sin(angle) * (cHitBoxBaseY - (cCrane.getY() + cCrane.getOriginY())) + (cCrane.getX() + cCrane.getOriginX()));
+		int newY = (int) (Math.sin(angle) * (cHitBoxBaseX - (cCrane.getX() + cCrane.getOriginX())) + Math.cos(angle) * (cHitBoxBaseY - (cCrane.getY() + cCrane.getOriginY())) + (cCrane.getY() + cCrane.getOriginY()));
+		cHitBox.x = newX;
+		cHitBox.y = newY;
+		x.setPosition(newX, newY);
 	}
 
 }
---
+++ core/src/cg/group4/game_logic/stroll/events/multiplayer/SmallFish.java
@@ -0,0 +1,46 @@
+package cg.group4.game_logic.stroll.events.multiplayer;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class SmallFish extends Image {
+	
+	protected int cDestinationX, cDestinationY;
+	protected int cSpeed = 4;
+	protected Rectangle cHitBox;
+	
+	public SmallFish(Texture t) {
+		super(t);
+		generateDestination();
+	}
+	
+	public void generateDestination() {
+		cDestinationX = (int) (Math.random() * (Gdx.graphics.getWidth() - this.getImageWidth()));
+		cDestinationY = (int) (Math.random() * (Gdx.graphics.getHeight() - this.getImageHeight()));
+	}
+	
+	public boolean destinationReached() {
+		boolean xCheck = cDestinationX <= (this.getX() + this.getImageWidth()) && cDestinationX >= this.getX();
+		boolean yCheck = cDestinationY <= (this.getY() + this.getImageHeight()) && cDestinationY >= this.getY();
+		
+		return xCheck && yCheck;
+	}
+	
+	public void move() {
+		int diffX = (int) (cDestinationX - this.getX());
+		int diffY = (int) (cDestinationY - this.getY());
+		
+		int newX = (int) (this.getX() + (cSpeed * Math.signum(diffX)));
+		int newY = (int) (this.getY() + (cSpeed * Math.signum(diffY)));
+//		System.out.println("CURRENTX: " + this.getX() + " CURRENTY: " + this.getY());
+//		System.out.println("NEWX: " + newX + " NEWY: " + newY);
+//		System.out.println("DESTINATION: X: " + cDestinationX + " Y: " + cDestinationY);
+		this.setPosition(newX, newY);
+	}
+	
+	public Rectangle getHitBox() {
+		return cHitBox;
+	}
+}
