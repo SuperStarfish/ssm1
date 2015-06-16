6a8f61296c9b6eed5bbd34cad44019236a2f1b0d
Jean <j.a.deleeuw@student.tudelft.nl>
2015-06-16 18:18:03 +0200

    Adjusted the CollectibleDrawer test to now work with the static methods

--- core/test/cg/group4/view/util/rewards/CollectibleDrawerTest.java
+++ core/test/cg/group4/view/util/rewards/CollectibleDrawerTest.java
@@ -22,39 +22,20 @@ import cg.group4.GdxTestRunner;
 
 @RunWith(GdxTestRunner.class)
 public class CollectibleDrawerTest {
-	
-	/**
-	 * The CollectibleDrawer to test.
-	 */
-	protected CollectibleDrawer cDrawer;
-	
-	
-	@Before
-	public void setUp() {
-		cDrawer = new CollectibleDrawer();
-	}
-
-	@After
-	public void tearDown() {
-		cDrawer = null;
-	}
 
 	@Test
 	public void fieldsTest() {
 		final float errorMargin = 0.00001f;
 		
 		final float replaceColourComponent = 0.4705882353f;
-		assertEquals(replaceColourComponent, cDrawer.cReplaceColourComponent, errorMargin);
-		
-		final float alpha = 1f;
-		assertEquals(alpha, cDrawer.cAlpha, errorMargin);
+		assertEquals(replaceColourComponent, CollectibleDrawer.cReplaceColourComponent, errorMargin);
 		
 		final Color replacementColour = new Color(
 				replaceColourComponent,
 				replaceColourComponent,
 				replaceColourComponent,
-				alpha);
-		assertEquals(replacementColour, cDrawer.cReplacementColour);
+				1f);
+		assertEquals(replacementColour, CollectibleDrawer.cReplacementColour);
 	}
 	
 	@Test
@@ -62,7 +43,7 @@ public class CollectibleDrawerTest {
 		final Color newColour = new Color(1f, 0f, 0f, 1f);
 		Pixmap pix = new Pixmap(new FileHandle(new File("test/testAssets/TestCollectibleDrawer1.png")));
 		pix.setColor(newColour);
-		cDrawer.replaceColours(pix);
+		CollectibleDrawer.replaceColours(pix);
 		
 		for (int y = 0; y < pix.getHeight(); y++) {
 			for (int x = 0; x < pix.getWidth(); x++) {
@@ -77,7 +58,7 @@ public class CollectibleDrawerTest {
 		final Color newColour = new Color(1f, 0f, 0f, 1f);
 		Pixmap pix = new Pixmap(new FileHandle(new File("test/testAssets/TestCollectibleDrawer2.png")));
 		pix.setColor(newColour);
-		cDrawer.replaceColours(pix);
+		CollectibleDrawer.replaceColours(pix);
 		
 		for (int y = 0; y < pix.getHeight(); y++) {
 			for (int x = 0; x < pix.getWidth(); x++) {
